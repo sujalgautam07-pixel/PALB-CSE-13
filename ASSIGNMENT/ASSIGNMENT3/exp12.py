f0a8faf653c35e24bb0d471585df2912082ee4e4
Sujal gautam <sujalgautam07@gmail.com>
2026-02-13 11:37:56 +0530

    Create exp12.py

---
+++ ASSIGNMENT/ASSIGNMENT3/exp12.py
@@ -0,0 +1,33 @@
+class Solution:
+    def getMinDiff(self, arr, k):
+        n = len(arr)
+        
+        if n == 1:
+            return 0
+        
+        arr.sort()
+        
+        ans = arr[-1] - arr[0]
+        
+        small = arr[0] + k
+        big = arr[-1] - k
+        
+        if small > big:
+            small, big = big, small
+        
+        for i in range(1, n - 1):
+            subtract = arr[i] - k
+            add = arr[i] + k
+            
+            if subtract < 0:
+                continue
+            
+            if subtract >= small or add <= big:
+                continue
+            
+            if big - subtract <= add - small:
+                small = subtract
+            else:
+                big = add
+        
+        return min(ans, big - small)
