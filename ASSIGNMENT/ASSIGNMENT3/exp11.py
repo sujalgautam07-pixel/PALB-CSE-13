54ce7ec82b15b372a3f5b3783111a1190481a614
Sujal gautam <sujalgautam07@gmail.com>
2026-02-13 11:37:19 +0530

    Create exp11.py

---
+++ ASSIGNMENT/ASSIGNMENT3/exp11.py
@@ -0,0 +1,13 @@
+import heapq
+
+class Solution:
+    def kthSmallest(self, arr, k):
+        max_heap = []
+        
+        for num in arr:
+            heapq.heappush(max_heap, -num)
+            
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
+        
+        return -max_heap[0]
